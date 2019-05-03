1a164f01b01f903cc7fd1b1aee6fd4c0f6b0f84f
Yigitcan Alanyali <y.alanyali@gmail.com>
2019-05-03 14:14:38 +0200

    Update mpv-updater-installer.bat
    Ignore SSL certificate warnings.
    Small changes.

--- mpv-updater-installer.bat
+++ mpv-updater-installer.bat
@@ -5,6 +5,21 @@ pushd %~dp0
 
 # PS Script below
 
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
 function Check-7z {
     $7zdir = (Get-Location).Path + "\7z"
     if (-not (Test-Path ($7zdir + "\7za.exe")))
